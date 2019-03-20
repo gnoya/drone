acfcc7a9a5116807bf4033677fce16e31919e991
Gabriel <gabrielnoya95@gmail.com>
2019-03-19 23:03:51 -0400

    codigo modulado y arreglado

--- arduino/test_imu_ros/test_imu_ros.ino
+++ arduino/test_imu_ros/test_imu_ros.ino
@@ -6,32 +6,20 @@
 #include <geometry_msgs/Vector3.h>
 #include <std_msgs/Int8.h>
 
+#define YAW 0
+#define PITCH 1
+#define ROLL 2
+#define THROTTLE 3
+
 int hertz = 200;
 int baudrate = 115200;
 
-int min_throttle_read = 986;
-int max_throttle_read = 1684;
-int min_yaw_read = 986;
-int max_yaw_read = 1684;
-int min_pitch_read = 986;
-int max_pitch_read = 1684;
-int min_roll_read = 986;
-int max_roll_read = 1684;
+int min_read_rc[4] = {986, 986, 986, 986};
+int max_read_rc[4] = {1684, 1684, 1684, 1684};
+int rc_pins[4] = {14, 15, 16, 17};
 
-int throttle_pin = 14;
-int yaw_pin = 15;
-int roll_pin = 16;
-int pitch_pin = 17;
-
-volatile uint16_t rc_throttle;
-volatile uint16_t rc_yaw;
-volatile uint16_t rc_pitch;
-volatile uint16_t rc_roll;
-
-uint16_t rc_throttle_timer;
-uint16_t rc_yaw_timer;
-uint16_t rc_pitch_timer;
-uint16_t rc_roll_timer;
+volatile uint16_t rc_values[4];
+uint16_t rc_timers[4];
 
 MPU6050 mpu;
 Quaternion q;
@@ -39,70 +27,18 @@ VectorFloat gravity;
 uint16_t packetSize;
 uint16_t fifoCount;
 uint8_t fifoBuffer[64];
-float ypr[3];
-float yaw, pitch, roll = 0;
+float angles[3];
 unsigned long timer = 0;
-unsigned long timer_2 = 0;
 float timeStep = 1 / (float)hertz;
 
-geometry_msgs::Vector3 angles;
+geometry_msgs::Vector3 ros_angles;
 std_msgs::Int8 pwm;
 ros::NodeHandle arduino_node;
-ros::Publisher chatter("imu", &angles);
+ros::Publisher chatter("imu", &ros_angles);
 ros::Publisher pub_pwm("pwm", &pwm);
 
-void throttle_handler(){
-  if(digitalRead(throttle_pin) == HIGH){
-    rc_throttle_timer = micros();
-    pwm.data = 2;
-    pub_pwm.publish(&pwm);
-  }
-  else {
-    rc_throttle = (uint16_t)(micros() - rc_throttle_timer);
-    if (rc_throttle < min_throttle_read) rc_throttle = min_throttle_read;
-    else if (rc_throttle > max_throttle_read) rc_throttle = max_throttle_read;
-    rc_throttle = (uint16_t)map(rc_throttle, min_throttle_read, max_throttle_read, 1000, 2000);
-  }
-  
-}
-
-void yaw_handler(){
-  if(digitalRead(yaw_pin) == HIGH){
-    rc_yaw_timer = micros();
-  }
-  else {
-    rc_yaw = (uint16_t)(micros() - rc_yaw_timer);
-    if (rc_yaw < min_yaw_read) rc_yaw = min_yaw_read;
-    else if (rc_yaw > max_yaw_read) rc_yaw = max_yaw_read;
-    rc_yaw = (uint16_t)map(rc_yaw, min_yaw_read, max_yaw_read, 1000, 2000);
-  }
-}
-
-void pitch_handler(){
-  if(digitalRead(pitch_pin) == HIGH){
-    rc_pitch_timer = micros();
-  }
-  else {
-    rc_pitch = (uint16_t)(micros() - rc_pitch_timer);
-    if (rc_pitch < min_pitch_read) rc_pitch = min_pitch_read;
-    else if (rc_pitch > max_pitch_read) rc_pitch = max_pitch_read;
-    rc_pitch = (uint16_t)map(rc_pitch, min_pitch_read, max_pitch_read, 1000, 2000);
-  }
-}
-
-void roll_handler(){
-  if(digitalRead(roll_pin) == HIGH){
-    rc_roll_timer = micros();
-  }
-  else {
-    rc_roll = (uint16_t)(micros() - rc_roll_timer);
-    if (rc_roll < min_roll_read) rc_roll = min_roll_read;
-    else if (rc_roll > max_roll_read) rc_roll = max_roll_read;
-    rc_roll = (uint16_t)map(rc_roll, min_roll_read, max_roll_read, 1000, 2000);
-  }
-}
-
-void setup() {
+void setup()
+{
   Wire.begin();
   mpu.initialize();
   mpu.dmpInitialize();
@@ -121,53 +57,119 @@ void setup() {
   arduino_node.advertise(chatter);
   arduino_node.advertise(pub_pwm);
 
-  pinMode(throttle_pin, INPUT);
-  pinMode(yaw_pin, INPUT);
-  pinMode(roll_pin, INPUT);
-  pinMode(pitch_pin, INPUT);
-  attachInterrupt(throttle_pin, throttle_handler, CHANGE);
-  attachInterrupt(yaw_pin, yaw_handler, CHANGE);
-  attachInterrupt(roll_pin, roll_handler, CHANGE);
-  attachInterrupt(pitch_pin, pitch_handler, CHANGE);
+  pinMode(rc_pins[YAW], INPUT);
+  pinMode(rc_pins[PITCH], INPUT);
+  pinMode(rc_pins[ROLL], INPUT);
+  pinMode(rc_pins[THROTTLE], INPUT);
+
+  attachInterrupt(rc_pins[YAW], yaw_handler, CHANGE);
+  attachInterrupt(rc_pins[PITCH], roll_handler, CHANGE);
+  attachInterrupt(rc_pins[ROLL], pitch_handler, CHANGE);
+  attachInterrupt(rc_pins[THROTTLE], throttle_handler, CHANGE);
 }
 
-void loop() {
+void loop()
+{
   timer = millis();
-  if (fifoCount < packetSize) {
-    angles.x = pitch;
-    angles.y = roll;
-    angles.z = yaw;
-    chatter.publish(&angles);
-    arduino_node.spinOnce();
-  }
-  else {
-    if (fifoCount == 1024) {
+  check_mpu();
+  ros_publish();
+
+  check_delay();
+}
+
+void check_mpu()
+{
+  // este fifo count estaba casi al final del loop.
+  fifoCount = mpu.getFIFOCount();
+
+  if (fifoCount >= packetSize)
+  {
+    if (fifoCount == 1024)
+    {
       mpu.resetFIFO();
     }
-    else{
-      if (fifoCount % packetSize != 0) {
+    else
+    {
+      if (fifoCount % packetSize != 0)
+      {
         mpu.resetFIFO();
-      } 
-      else {
-        while (fifoCount >= packetSize) {
+      }
+      else
+      {
+        while (fifoCount >= packetSize)
+        {
           mpu.getFIFOBytes(fifoBuffer, packetSize);
           fifoCount -= packetSize;
         }
-      
+
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        yaw = ypr[0] * 180/PI;
-        pitch = ypr[1] * 180/PI;
-        roll = ypr[2] * 180/PI;
+        mpu.dmpGetYawPitchRoll(angles, &q, &gravity);
+        // Cuidado si esto altera los valores, antes no se sobreescribia a si mismo.
+        angles[YAW] = angles[YAW] * 180 / PI;
+        angles[PITCH] = angles[PITCH] * 180 / PI;
+        angles[ROLL] = angles[ROLL] * 180 / PI;
       }
     }
   }
+}
 
-  fifoCount = mpu.getFIFOCount();
-  
-  if((timeStep * 1000) - (millis() - timer) > 0){
-    delay((timeStep * 1000) - (millis() - timer));
+void ros_publish()
+{
+  ros_angles.x = angles[ROLL];
+  ros_angles.y = angles[PITCH];
+  ros_angles.z = angles[YAW];
+  chatter.publish(&ros_angles);
+  arduino_node.spinOnce();
+}
+
+void check_delay()
+{
+  unsigned long time_left = 1000 * timeStep - (millis() - timer);
+  if (time_left > 0)
+  {
+    delay(time_left);
   }
+}
 
+void yaw_handler()
+{
+  rc_timing(YAW);
+}
+
+void pitch_handler()
+{
+  rc_timing(PITCH);
+}
+
+void roll_handler()
+{
+  rc_timing(ROLL);
+}
+
+void throttle_handler()
+{
+  rc_timing(THROTTLE);
+}
+
+void rc_timing(int channel)
+{
+  if (digitalRead(rc_pins[channel]) == HIGH)
+  {
+    rc_timers[channel] = micros();
+  }
+  else
+  {
+    rc_values[channel] = rc_map(rc_timers[channel], min_read_rc[channel], max_read_rc[channel]);
+  }
+}
+
+uint16_t rc_map(uint16_t timer, int min, int max)
+{
+  uint16_t value = (uint16_t)(micros() - timer);
+  if (value < min)
+    value = min;
+  else if (value > max)
+    value = max;
+  return (uint16_t)map(value, min, max, 1000, 2000);
 }
